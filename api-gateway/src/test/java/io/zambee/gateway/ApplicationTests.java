066e103ef5e594604517e925984f2421b6ffc39a
Dmytro Karmanov <dmytro.karmanov@auto1.com>
2016-07-17 02:39:00 +0200

    sample

---
+++ api-gateway/src/test/java/io/zambee/gateway/ApplicationTests.java
@@ -0,0 +1,18 @@
+package io.zambee.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
